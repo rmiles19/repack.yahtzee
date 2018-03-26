67638b526427ed1d94281f7eca97109e5eb701ee
Rachel Miles <milesrachel9@gmail.com>
2018-03-26 12:20:05 -0600

    add roll, keep function

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import React from 'react'
+import { 
+  Grid, 
+  Button, 
+  Divider, 
+} from 'semantic-ui-react'
+import Dice from './Dice'
 
-const Board = () => <div>Board</div>
+const Board = ({ 
+  roll, 
+  dice, 
+  rollDice, 
+  keep,
+  toggleKept,
+}) => {
+  const maxRoll = roll === 3;
+  const disabled = maxRoll ? { disabled: true } : {}
+  return (
+    <Grid>
+      <Grid.Row>
+        <Button
+          fluid
+          onClick={rollDice}
+          {...disabled}
+        >
+          { maxRoll ? 'Score Roll' : 'Roll Dice' }
+        </Button>
+        <Grid.Column width={16}>
+          <Divider hidden />
+        </Grid.Column>
+        { roll > 0 &&
+            dice.map( (d,i) => {
+              const kept = keep.includes(i)
+              return (
+                <Dice
+                  key={i}
+                  value={d}
+                  kept={kept}
+                  toggleKept={toggleKept}
+                  index={i}
+                />
+              )
+            })
+          }
+      </Grid.Row>
+    </Grid>
+  )
+}
 
-export default Board 
+export default Board
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,31 +1,75 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
-import Board from './Board';
-import ScoreCard from './ScoreCard';
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import Board from './Board'
+import ScoreCard from './ScoreCard'
 
 const styles = {
-  fullHeight: { height: '100vh' }, 
-  board: { backgroundColor: '#AAFFAA'}, 
-  scores: { backgroundColor: '#9370DB'}, 
+  fullHeight: { height: '100vh' },
+  board: { backgroundColor: '#AAFFAA' },
+  scores: { backgroundColor: '#9370DB' },
 }
 
-const Game = () => (
-  <Grid>
-    <Grid.Row>
-      <Grid.Column
-        width={10}
-        style={{...styles.fullHeight, ...styles.board }}
-      >
-        <Board />
-      </Grid.Column>
-      <Grid.Column
-        width={6}
-        style={{...styles.fullHeight, ...styles.scores}}
-      >
-        <ScoreCard />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
+class Game extends React.Component {
+  state = { 
+    roll: 0,
+    dice: [...new Array(5)],
+    keep: [], 
+  }
 
-export default Game 
+  toggleKept = (i) => {
+    const { keep } = this.state;
+    let updatedKeep;
+
+    if (keep.includes(i))
+      updatedKeep = keep.filter( k => k !== i )
+    else
+      updatedKeep = [...keep, i]
+
+    this.setState({ keep: updatedKeep })
+  }
+
+  rollDice = () => {
+    const { keep } = this.state;
+
+    const dice = this.state.dice.map( (el, i) => {
+      if (keep.includes(i))
+        return el
+      return Math.floor(Math.random() * 6) + 1
+    });
+
+    this.setState( state => {
+      return { dice, roll: state.roll + 1 }
+    })
+  }
+
+  render() {
+    const { roll, dice, keep } = this.state;
+
+    return (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column
+            width={10}
+            style={{...styles.fullHeight, ...styles.board }}
+          >
+            <Board 
+              roll={roll}
+              dice={dice}
+              rollDice={this.rollDice}
+              keep={keep}
+              toggleKept={this.toggleKept}
+            />
+          </Grid.Column>
+          <Grid.Column
+            width={6}
+            style={{...styles.fullHeight, ...styles.scores}}
+          >
+            <ScoreCard />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}
+
+export default Game
